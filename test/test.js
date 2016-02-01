d3688b2f65fb604d5480877c37cade1b74922476
Bartosz Kozera <bartek.kozera@gmail.com>
2016-02-01 17:06:12 +0100

    adding some more tests

--- test/test.js
+++ test/test.js
@@ -257,6 +257,16 @@ describe('vanish grid function', function() {
             [0, 0, 0]
         ])
     })
+
+    it('should vanish two rows at the same time', function() {
+        expect(vanish.grid([
+            [0, 1, 1, 1],
+            [1, 2, 2, 2]
+        ])).to.deep.equal([
+            [0, 0, 0, 0],
+            [1, 0, 0, 0]
+        ])
+    })
 })
 
 describe('clear grid function', function() {
@@ -270,7 +280,7 @@ describe('clear grid function', function() {
         ])
     })
 
-    it('should work for some complex cases', function() {
+    it('should work for some simple cases', function() {
         expect(solver.clearGrid([
             [1, 0, 1, 0],
             [0, 1, 0, 0],
@@ -298,6 +308,19 @@ describe('clear grid function', function() {
             [0, 0, 0, 0],
             [0, 0, 0, 0]
         ]);
+    })
+    it('should work for more complex cases', function() {
+        expect(solver.clearGrid([
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 2, 3, 1, 3, 0],
+            [2, 2, 4, 1, 5, 3, 0],
+            [4, 4, 5, 5, 1, 1, 1]
+        ])).to.deep.equal([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 3, 1, 3, 0],
+            [0, 0, 0, 1, 1, 3, 0]
+        ])
     })
 })
 
