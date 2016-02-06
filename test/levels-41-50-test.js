d6bfe59aa07d1eaa07f86917e23bfd0708441881
Bartek Kozera <bartek.kozera@gmail.com>
2016-02-06 14:19:28 +0100

    adding tests for levels up to 50

--- test/levels-41-50-test.js
+++ test/levels-41-50-test.js
@@ -106,6 +106,115 @@ describe('find solution function for levels 41-50', function() {
 
     })
 
+    it('should find a solution for level 46 in two steps', function() {
+        var solution = solver.solve([
+            [2, 0, 0],
+            [1, 0, 0],
+            [3, 0, 2],
+            [2, 0, 3],
+            [3, 1, 1]
+        ], 2);
+
+        expect(solution).to.deep.equal([{
+            x: 4,
+            y: 3,
+            direction: 'left'
+        }, {
+            x: 2,
+            y: 3,
+            direction: 'right'
+        }])
+
+    })
+
+    it('should find a solution for level 47 in two steps', function() {
+        var solution = solver.solve([
+            [0, 1, 0, 0],
+            [0, 2, 0, 0],
+            [1, 1, 0, 0],
+            [2, 1, 0, 0],
+            [1, 2, 0, 0],
+            [1, 2, 0, 1]
+        ], 2);
+
+        expect(solution).to.deep.equal([{
+            x: 3,
+            y: 3,
+            direction: 'right'
+        }, {
+            x: 2,
+            y: 3,
+            direction: 'right'
+        }])
+
+    })
+
+    it('should find a solution for level 48 in two steps', function() {
+        var solution = solver.solve([
+            [2, 2, 1],
+            [1, 1, 2],
+            [2, 2, 1],
+            [1, 1, 2],
+            [2, 2, 1],
+            [1, 1, 2]
+        ], 2);
+
+        expect(solution).to.deep.equal([{
+            x: 4,
+            y: 5,
+            direction: 'right'
+        }, {
+            x: 5,
+            y: 5,
+            direction: 'left'
+        }])
+
+    })
+
+    it('should find a solution for level 49 in two steps', function() {
+        var solution = solver.solve([
+            [0, 1, 0],
+            [0, 1, 2],
+            [1, 3, 2],
+            [2, 1, 3],
+            [1, 3, 2],
+            [1, 1, 2]
+        ], 2);
+
+        expect(solution).to.deep.equal([{
+            x: 2,
+            y: 3,
+            direction: 'right'
+        }, {
+            x: 3,
+            y: 3,
+            direction: 'right'
+        }])
+
+    })
+
+    it('should find a solution for level 50 in two steps', function() {
+        var solution = solver.solve([
+            [3, 0, 1, 0, 0],
+            [1, 3, 3, 0, 0],
+            [2, 1, 4, 0, 0],
+            [1, 2, 2, 0, 0],
+            [4, 4, 1, 0, 1]
+        ], 2);
+
+        expect(solution).to.deep.equal([{
+            x: 2,
+            y: 3,
+            direction: 'left'
+        }, {
+            x: 4,
+            y: 2,
+            direction: 'right'
+        }])
+
+    })
+
+
 
 
 
